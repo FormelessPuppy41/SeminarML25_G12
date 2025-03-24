3d6eb507e7700b62770c2efa231ef58fc4fa6d41
BQ <berend.quist@gmail.com>
2025-03-24 13:26:09 +0100

    Added the forecast writer and data loader classes.

---
+++ data/data_loader.py
@@ -0,0 +1,67 @@
+"""
+Data loader for the datasets
+"""
+import pandas as pd
+
+
+class DataLoader:
+    def __init__(self):
+        self.path = 'data/data_files/'
+
+    def load_input_data(self, file_name: str):
+        """
+        This function loads the input data from a CSV file.
+
+        Args:
+            file_name (str): The file name for the input data.
+
+        Returns:
+            pd.DataFrame: The input data.
+        """
+        path = f'{self.path}/input_files/{file_name}'
+        return self._load_data(path)
+    
+    def load_output_data(self, file_name: str):
+        """
+        This function loads the output data from a CSV file.
+
+        Args:
+            file_name (str): The file name for the output data.
+
+        Returns:
+            pd.DataFrame: The output data.
+        """
+        path = f'{self.path}/output_files/{file_name}'
+        return self._load_data(path)
+    
+    def load_model_results(self, file_name: str):
+        """
+        This function loads the model results from a CSV file.
+
+        Args:
+            file_name (str): The file name for the model results.
+
+        Returns:
+            pd.DataFrame: The model results.
+        """
+        path = f'{self.path}/model_results/{file_name}'
+        return self._load_data(path)
+
+    
+    def _load_data(self, path: str):
+        """
+        This function loads the data from a CSV file.
+
+        Args:
+            path (str): The path to the CSV file.
+
+        Returns:
+            pd.DataFrame: The data.
+        """
+        # check the file type to use either csv or xlsx
+        if path.endswith('.csv'):
+            return pd.read_csv(path)
+        elif path.endswith('.xlsx'):
+            return pd.read_excel(path)
+        else:
+            raise ValueError('File type not supported')
