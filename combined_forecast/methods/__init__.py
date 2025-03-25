e82460caa2b7b4689a559cb272ecce458dc3340e
BQ <berend.quist@gmail.com>
2025-03-25 13:08:08 +0100

    improved the adaptive elastic net and added better output logs when running the methods as mains.

--- combined_forecast/methods/__init__.py
+++ combined_forecast/methods/__init__.py
@@ -2,4 +2,4 @@
 from .ridge import run_ridge
 from .lasso import run_lasso
 from .elastic_net import run_elastic_net
-from .adaptive_elastic_net import run_adaptive_elastic_net
+from .adaptive_elastic_net import run_day_ahead_adaptive_elastic_net
